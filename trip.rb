0fa222503a64d9a7f925ed71e40f3e47d8017b0c
ssstroud23 <ssstroud23@gmail.com>
2018-07-10 18:04:19 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -10,8 +10,6 @@ stay = gets.chomp.capitalize  #don't forget to Capitilize the input
 
 # Do the same thing with `eats` and one last time with `nights`, capturing their text input and storing them to the respective local variable
 
-eats = gets.chomp.capitalize
-nights = gets.chomp.capitalize
 
 # Lastly, puts it all back by interpolating these values in a string.
 
