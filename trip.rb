55616dd5ac0a01e1d0a5778e9cac8e03b8afacda
ssstroud23 <ssstroud23@gmail.com>
2018-07-10 18:14:56 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -20,7 +20,7 @@ puts "What would you like to eat?"
 eat = gets.chomp
 puts "I would like to eat #{eat}"
 puts "How many nights are you staying?"
-staying = gets.chomp
+nights = gets.chomp
 puts "I would like to stay 4 #{nights}"
 
 puts "I would like to stay in a #{hotel}, eat #{pizza} and stay 4 #{nights}"
