a2af9ac7b859bd44120a21373ddea182d1a9de4d
ssstroud23 <ssstroud23@gmail.com>
2018-07-10 18:01:20 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -4,6 +4,7 @@
 puts "Let's plan your trip! Where would you like to stay?"
 
 # Then, set a variable called stay to get the user's input. Make sure to capitilize that input.
+stay = "gets.chomp".capitalize
 
 stay = gets.chomp.capitalize  #don't forget to Capitilize the input
 
@@ -13,5 +14,4 @@ stay = gets.chomp.capitalize  #don't forget to Capitilize the input
 
 # Lastly, puts it all back by interpolating these values in a string.
 
-puts "Where would you like to stay?"
-  gets
+puts ""
